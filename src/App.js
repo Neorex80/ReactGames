84bb9fd6faae54220acab8e1d23eaa70ab280203
Rushikesh Pawar <125253603+Neorex80@users.noreply.github.com>
2024-08-13 00:07:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import GameBoard from './components/GameBoard';
+import SnakeGame from './components/SnakeGame';
+import Home from './components/Home';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Tic Tac Toe</h1>
-      <GameBoard />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tic-tac-toe" element={<GameBoard />} />
+          <Route path="/snake-game" element={<SnakeGame />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
